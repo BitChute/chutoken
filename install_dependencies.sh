3890adf6bd70df14e2bf4b583ff484b053c5c6de
Ray Vahey <info@chutoken.com>
2018-02-15 12:53:11 +0000

    dependencies script

---
+++ install_dependencies.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install -y build-essential cmake pkg-config libboost-all-dev libssl-dev libunbound-dev libminiupnpc-dev libunwind8-dev liblzma-dev libldns-dev libexpat1-dev doxygen graphviz
